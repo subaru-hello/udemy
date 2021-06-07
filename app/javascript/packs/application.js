556a59cba30b4cb1ab984198c2dbeb03ba9e63fa
subaru-one <subaru@SubarunoMacBook-puro-3.local>
2021-06-07 12:06:40 +0900

    Questions コントローラーの作成 #1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,22 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('bootstrap/dist/js/bootstrap.min.js')
+equire("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require('bootstrap/dist/js/bootstrap.min.js')
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
 
 Rails.start()
 Turbolinks.start()
